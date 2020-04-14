809125783c98cdd6d841a2ae2099d1eb4c80dd10
Tatar9 <51324444+Tatar9@users.noreply.github.com>
2020-04-14 16:55:25 +0200

    connexion, cookie et deconnexion fonctionnent

--- code/LoginTest.php
+++ code/LoginTest.php
@@ -1,31 +1,4 @@
-<?php
-	//recuperer les données venant de la page HTML
-	//identifier votre BDD
-	$database = "piscine_users";
-	//connectez-vous dans votre BDD
-	//Rappel: votre serveur = localhost et votre login = root et votre password = <rien>
-	$db_handle = mysqli_connect('localhost', 'root', '');
-	$db_found = mysqli_select_db($db_handle, $database);
-	if ($_POST['submit']) {
-		$prenom = $_POST["prenom"];
-		$nom = $_POST["nom"];
-		if ($db_found) {
-			$sql = "SELECT * FROM `users` WHERE Nom LIKE '$nom' AND Pays LIKE '$prenom'" ;
-			$result = mysqli_query($db_handle, $sql);
-			if (mysqli_num_rows($result)==0)
-			{
-				echo "pas trouvé";
-			}
-			else
-			{
-				echo "trouvé";
-			}
-			
-		}
-	}
-	mysqli_close($db_handle);
 
-?>
 
 <html>
 	<head>
@@ -34,9 +7,55 @@
 	</head>
 	<body>
 		<form name ="form" method ="POST" action = "LoginTest.php">
-			<input type = "text" name ="prenom">
-			<input type = "text" name ="nom">
+			Nom :
+			<input type = "text" name ="nom"><br>
+			Mot de passe :
+			<input type="password" name="mdp"<br>
 			<input type = "submit" name = "submit" value = "Se connecter">
 		</form>
 	</body>
 </html>
+
+<?php
+	$database = "piscine_users";
+	$db_handle = mysqli_connect('localhost', 'root', '');
+	$db_found = mysqli_select_db($db_handle, $database);
+	if(isset($_POST['submit']))
+	{
+		if ($_POST['submit']) {
+			$mdp = $_POST["mdp"];
+			$nom = $_POST["nom"];
+			if ($db_found) {
+				$sql = "SELECT * FROM `users` WHERE Nom LIKE '$nom' AND mdp LIKE '$mdp'" ;
+				$result = mysqli_query($db_handle, $sql);
+				if (mysqli_num_rows($result)==0)
+				{
+					echo "pas trouvé";
+				}
+				else
+				{
+					echo "trouvé";
+					while ($data = mysqli_fetch_assoc($result)) {
+						$c_ID = $data['ID'];
+						$c_nom = $data['Nom'];
+						$c_mdp = $data['mdp'];
+						$c_pays = $data['Pays'];
+						$c_rang = $data['rang'];
+						$fp = fopen('cookie.php', 'w');
+
+						fwrite($fp, "<?php 
+							define('C_ID', '$c_ID'); 
+        	        		define('C_PAYS', '$c_pays');
+        	        	?>");
+						fclose($fp);
+        	        	header("location: PageTestCookie.php");
+        	        	
+					}
+				}
+				
+			}
+		}
+	}
+	
+	mysqli_close($db_handle);
+?>
---
+++ code/PageTestCookie.php
@@ -0,0 +1,63 @@
+<html>
+	<head>
+		<title>PageTestCookie</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<h3>Bienvenue sur la page test du cookie</h3>
+
+		<div class= "infoCompte">
+			<?php
+				require "cookie.php";
+				if (filesize("cookie.php") != 0)
+				{
+					$database = "piscine_users";
+					//connectez-vous dans votre BDD
+					//Rappel: votre serveur = localhost et votre login = root et votre password = <rien>
+					$db_handle = mysqli_connect('localhost', 'root', '');
+					$db_found = mysqli_select_db($db_handle, $database);
+					$id2=C_ID;
+					$sql = "SELECT * FROM `users` WHERE ID LIKE '$id2'";
+					$result = mysqli_query($db_handle, $sql);
+					while ($data = mysqli_fetch_assoc($result)) {
+						$c_ID = $data['ID'];
+						print("<br>Votre ID est : ");
+						print($c_ID);
+
+						$c_nom = $data['Nom'];
+						print("<br>Votre Nom est : ");
+						print($c_nom);
+
+						$c_mdp = $data['mdp'];
+
+						$c_pays = $data['Pays'];
+						print("<br>Votre pays est : ");
+						print($c_pays);
+
+						$c_rang = $data['rang'];
+						print("<br>Votre rang est : ");
+						print($c_rang);
+
+
+					}
+				}
+			?>
+		</div>
+		<br>
+		<br>
+		<form name ="form" method ="POST" action = "PageTestCookie.php">
+			<input type = "submit" name = "deco" value = "Déconnexion">
+		</form>
+	</body>
+</html>
+
+<?php
+	if(isset($_POST['deco']))
+	{
+		if ($_POST['deco']) {
+			$fp = fopen('cookie.php', 'w');
+			fclose($fp);
+			header("location: LoginTest.php");
+		}
+	}
+?>
